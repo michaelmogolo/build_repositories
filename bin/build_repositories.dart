fd88266b00123912136fa62ba3711900563ee3b9
MogoloMG08 <52840616+michaelmogolo@users.noreply.github.com>
2022-11-14 09:44:41 +0100

    first commit

---
+++ bin/build_repositories.dart
@@ -0,0 +1,5 @@
+import 'package:build_repositories/build_repositories.dart' as build_repositories;
+
+void main(List<String> arguments) {
+  print('Hello world: ${build_repositories.calculate()}!');
+}
